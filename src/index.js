bad6fc522e958993849ca4508429088f4a29ad3f
Mark Muskardin <mmuskardin@trulia.com>
2016-01-14 16:24:16 -0800

    showing input value in search box and created OptionsTemplate react component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import JSONP from 'jsonp';
 import Typeahead from 'react-typeahead-component';
+import OptionsTemplate from './OptionsTemplate.jsx'
 
 const googleAutoSuggestURL = '//suggestqueries.google.com/complete/search?client=youtube&ds=yt&q=';
 
@@ -12,15 +13,31 @@ export default React.createClass({
     };
   },
 
+  handleOptionsChange(event, options) {
+    var inputValue = value[0];
+    this.setState({
+        inputValue: inputValue
+    });
+  },
+
   handleChange(event) {
+    var self = this;
     var query = event.target.value;
     var url = googleAutoSuggestURL + query;
+
+    this.setState({
+      inputValue: query
+    });
 
     JSONP(url, function(error, data) {
       if (error) {
           reject(error);
       } else {
+        var searchResults = data;
 
+        self.setState({
+          options: searchResults
+        });
       }
     });
   },
@@ -32,6 +49,8 @@ export default React.createClass({
         options={this.state.options}
         placeholder='Search'
         onChange={this.handleChange}
+        optionTemplate={OptionsTemplate}
+        onOptionChange={this.handleOptionChange} 
       />
       <h2>Welcome to React components</h2>
     </div>
